6e0571eabdc492625a37c299ac89ddc5fb989355
QueenterJuma <jumahqueenter@gmail.com>
2023-01-30 20:56:46 +0300

    Add additional event listener

--- script.js
+++ script.js
@@ -112,6 +112,12 @@ hamburger.addEventListener("click", () => {
   mobileMenu.classList.add("show");
   body.classList.add("overflowHidden");
 });
+
+cancel.addEventListener("click", () => {
+  mobileMenu.classList.remove("show");
+  body.classList.remove("overflowHidden");
+});
+
 mobileLinks.forEach((item) => {
   item.addEventListener("click", () => {
     mobileMenu.classList.remove("show");
